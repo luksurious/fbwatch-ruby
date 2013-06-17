1ade07e9024a8f7999caf59c4f63d1fa9656618b
luksurious <lukas@brueckner-web.de>
2013-06-17 23:18:05 +0200

    first ruby on rails thing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbwatch::Application.config.secret_token = 'f00a7d296b5ff73c901b30185d1b2f92f9c93ce924ac73b3bc6546695d5161d463e11b79c9552e80d6e1cc73e098ccef275de1e192dd48e864b7378bcc824da9'
