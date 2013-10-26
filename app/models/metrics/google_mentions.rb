a979f0afc44b730af452527e7b01554850df650a
Lukas Brückner <lukas@brueckner-web.de>
2013-10-26 17:52:53 +0200

    add google search mentions

---
+++ app/models/metrics/google_mentions.rb
@@ -0,0 +1,53 @@
+require "net/http"
+require "uri"
+
+module Metrics
+  class GoogleMentions < MetricBase
+    def analyze
+
+      resource_combinations(2).each do |combination|
+
+        # calc shared resources
+        token = get_combination_token(combination)
+
+        web_results = query_google_keywords(keywords_for(combination))
+
+        make_group_metric_model(name: 'google_mentions', token: token, value: web_results, resources: combination)
+      end
+    end
+
+    def keywords_for(resources)
+      return [] unless resources.is_a?(Array)
+
+      keywords = []
+      resources.each do |res|
+        # self.keywords is defined in MetricBase
+        keywords << self.keywords[res.id]
+      end
+
+      keywords
+    end
+
+    def query_google_keywords(keywords)
+      query_parameters = []
+      keywords.each do |group|
+        query_parameters << '("' << group.join('" OR "') << '")'
+      end
+
+      Rails.logger.debug("Calling google with query: #{query_parameters.join(' AND ')}")
+
+      uri = URI.parse("http://www.google.com/search?hl=en&q=#{URI.escape(query_parameter)}&filter=0")
+      response = Net::HTTP.get_response(uri)
+
+      html_count = response.body.match(/id\=\"resultStats\"\>[^\<]+/)
+
+      return 0 if html_count.nil? or html_count.length == 0
+
+      inner_html = html_count[0].match(/[0-9,\.]+/)
+
+      return 0 if inner_html.nil? or inner_html.length == 0
+      
+      inner_html[0].gsub(/[,\.]/, '')
+    end
+  end
+end
