5d8409b91b614bfed95786bf1693e3cd2587f797
Lukas Brückner <lukas@brueckner-web.de>
2013-11-09 14:25:04 +0100

    show small graph on resource page

--- app/controllers/network_graph_controller.rb
+++ app/controllers/network_graph_controller.rb
@@ -9,61 +9,47 @@ class NetworkGraphController < ApplicationController
     @color_map.sample
   end
 
+  def for_resource
+    resource = Resource.find(params[:id])
+    group = ResourceGroup.find(params[:group_id])
+
+    group_graph(group, 'network_graph', resource)
+  end
+
   def for_resource_group
-    resource_group = ResourceGroup.find(params[:id])
-    json = {
-      nodes: [],
-      edges: []
-    }
-
-    GroupMetric.where(resource_group_id: params[:id], metric_class: 'network_graph', name: 'graph_node').each do |metric|
-      json[:nodes] << {
-        id: metric.resource.username,
-        label: metric.resource.name,
-        size: [metric.value, 1].max,
-        color: random_color,
-        forceLabel: metric.value > 5
-      }
-    end
-
-    GroupMetric.where(resource_group_id: params[:id], metric_class: 'network_graph', name: 'graph_edge').each do |metric|
-      next if metric.resources.empty?
-      json[:edges] << {
-        source: metric.resources.first.username,
-        target: metric.resource.username,
-        weight: [metric.value, 1].max
-      }
-    end
-
-    respond_to do |format|
-      format.html { redirect_to resource_group_details_path(resource_group) }
-      format.json { render json: json }
-    end
+    group_graph(ResourceGroup.find(params[:id]), 'network_graph')
   end
 
   def google_for_resource_group
-    resource_group = ResourceGroup.find(params[:id])
+    group_graph(ResourceGroup.find(params[:id]), 'network_graph_google')
+  end
+
+  def group_graph(resource_group, metric_class, resource = nil)
     json = {
       nodes: [],
       edges: []
     }
 
-    GroupMetric.where(resource_group_id: params[:id], metric_class: 'network_graph_google', name: 'graph_node').each do |metric|
+    GroupMetric.where(resource_group_id: resource_group.id, metric_class: metric_class, name: 'graph_edge').each do |metric|
+      # skip malformed items
+      next if metric.resources.empty?
+      # if getting the graph for a specific resource skip unrelated edges
+      next if !resource.nil? and metric.resources.first != resource and metric.resource != resource
+
+      json[:edges] << {
+        source: metric.resources.first.username,
+        target: metric.resource.username,
+        weight: [metric.value, 1].max
+      }
+    end
+
+    GroupMetric.where(resource_group_id: resource_group.id, metric_class: metric_class, name: 'graph_node').each do |metric|
       json[:nodes] << {
         id: metric.resource.username,
         label: metric.resource.name,
         size: [metric.value, 1].max,
         color: random_color,
         forceLabel: metric.value > 5
-      }
-    end
-
-    GroupMetric.where(resource_group_id: params[:id], metric_class: 'network_graph_google', name: 'graph_edge').each do |metric|
-      next if metric.resources.empty?
-      json[:edges] << {
-        source: metric.resources.first.username,
-        target: metric.resource.username,
-        weight: [metric.value, 1].max
       }
     end
 
