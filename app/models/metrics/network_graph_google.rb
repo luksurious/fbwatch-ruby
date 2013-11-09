4927b7bb1d83995703c63c946fef11fce6aeebdc
Lukas Brückner <lukas@brueckner-web.de>
2013-11-09 15:06:11 +0100

    better google graph calculation using clusters

--- app/models/metrics/network_graph_google.rb
+++ app/models/metrics/network_graph_google.rb
@@ -5,27 +5,66 @@ module Metrics
 
       all_metrics = GroupMetric.where(resource_group_id: @resource_group.id, metric_class: 'google_mentions', name: 'google_mentions').
                                 # group by source
-                                order(:resource_id).group_by(&:resource_id)
+                                order(:resource_id)
+
+      all_scores = []
+
+      all_metrics.each do |metric|
+        all_scores << metric.sort_value if metric.sort_value >= 1
+      end
+
+      upper_boundary = 10 ** Math.log(all_scores.max, 10).round() * 1.0
+      @clusters = [
+        {
+          max: upper_boundary - 1,
+          min: upper_boundary / 10,
+          score_max: 10,
+          score_min: 5
+        },
+        {
+          max: upper_boundary / 10 - 1,
+          min: upper_boundary / 100,
+          score_max: 5,
+          score_min: 2.5
+        },
+        {
+          max: upper_boundary / 100 - 1,
+          min: upper_boundary / 1000,
+          score_max: 2.5,
+          score_min: 1
+        }
+      ]
+
+      #crazy_mean = Stats.geometric_mean([Stats.geometric_mean(all_scores), all_scores.reduce(&:+) / all_scores.length])
+      #geometric_mean = Stats.geometric_mean(all_scores.map { |x| (x < crazy_mean/1000 or x > crazy_mean*1000) ? nil : x }.compact)
+
+      #deviation = confidence_deviation(all_scores, geometric_mean)
+      #lower_boundary = [geometric_mean / 1000, all_scores.min].max
+      #upper_boundary = [geometric_mean * 1000, all_scores.max].min
 
       graph_score = {
         edges: {},
         nodes: {}
       }
 
-      all_metrics.each do |res_id, group|
+      all_metrics.group_by(&:resource_id).each do |res_id, group|
         graph_score[:nodes][res_id] ||= []
 
         group.each do |metric|
           next if metric.self_referencing?
           target_id = metric.resources.first.id
 
+          normalized_score = cluster_value(metric.sort_value)
+          #normalized_score = normalize_weight([metric.sort_value - lower_boundary, 0].max, geometric_mean - lower_boundary)
+          #[[((metric.sort_value - lower_boundary) / (upper_boundary - lower_boundary)), 0].max, 1].min * 10
+
           graph_score[:nodes][target_id] ||= []
-          graph_score[:nodes][target_id] << metric.sort_value
-          graph_score[:nodes][res_id] << metric.sort_value
+          graph_score[:nodes][target_id] << normalized_score
+          graph_score[:nodes][res_id] << normalized_score
 
           graph_score[:edges][ [res_id, target_id].min ] ||= {}
           graph_score[:edges][ [res_id, target_id].min ][ [res_id, target_id].max ] ||= []
-          graph_score[:edges][ [res_id, target_id].min ][ [res_id, target_id].max ] << metric.sort_value
+          graph_score[:edges][ [res_id, target_id].min ][ [res_id, target_id].max ] << normalized_score
         end
       end
 
@@ -62,5 +101,41 @@ module Metrics
     def sort_value(value)
       value
     end
+
+    private
+      def cluster_value(value)
+        @clusters.each do |cluster|
+          if value >= cluster[:min] and value <= cluster[:max]
+            return (value - cluster[:min]) / (cluster[:max] - cluster[:min]) * (cluster[:score_max] - cluster[:score_min]) + cluster[:score_min]
+          end
+        end
+
+        value == 0 ? 0.1 : 1.0
+      end
+
+      def confidence_deviation(values, geometric_mean)
+        # TODO this loop is pretty awful
+        percentage_included = 0
+        deviation = 1.1
+        loop do
+          percentage_included = values_in_interval(values, geometric_mean / deviation, geometric_mean * deviation) / values.length
+          break if percentage_included >= 0.8
+          deviation *= 1.1
+        end
+
+        deviation
+      end
+
+      def values_in_interval(values, from, to)
+        count = 0
+        values.each do |x|
+          count += 1 if x >= from and x <= to
+        end
+        count
+      end
+
+      def normalize_weight(value, center)
+        value + Math.sqrt(2* (center**2)) - Math.sqrt((value - center) ** 2 + (center ** 2))
+      end
   end
 end
