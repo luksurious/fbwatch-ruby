bd8a3db7749ab56f8c336594d75d5399e34d0fd0
Lukas Brückner <lukas.brueckner@kyto.com>
2013-11-29 11:44:59 +0100

    hopefully avoid google bot detection

--- app/models/metrics/google_mentions.rb
+++ app/models/metrics/google_mentions.rb
@@ -4,6 +4,15 @@ require 'sanitize'
 
 module Metrics
   class GoogleMentions < MetricBase
+    user_agents = [
+      'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0',
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 6.0) yi; AppleWebKit/345667.12221 (KHTML, like Gecko) Chrome/23.0.1271.26 Safari/453667.1221',
+      'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+      'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0; chromeframe/13.0.782.215)',
+      'Opera/12.80 (Windows NT 5.1; U; en) Presto/2.10.289 Version/12.02'
+    ]
+
     def analyze
       clear
       @logger = Logger.new("#{Rails.root}/log/google_mentions.log")
@@ -11,18 +20,13 @@ module Metrics
       resource_combinations(2).each do |combination|
 
         # calc shared resources
-        combi_keywords = keywords_for(combination)
-
-        for i in 1..5
-          web_results = query_google_keywords(combi_keywords)
-
-          break if web_results[:count] > 0
-          # if absolutely no results were found wait a second and try again, there seem to be some limits for automating queries
-          sleep 1
-        end
+        web_results = query_google_keywords(keywords_for(combination))
 
         make_mutual_group_metric_model(name: 'google_mentions', value: web_results, resources: combination)
         @logger.debug "-- count #{web_results[:count]}"
+
+        # wait for some time to avoid detection
+        sleep Random.rand(0..30)
       end
     end
 
@@ -58,7 +62,7 @@ module Metrics
 
       @logger.debug("Calling google with query: #{query_parameter}")
 
-      response = fetch("http://www.google.com/search?hl=en&q=#{URI.escape(query_parameter)}&filter=0")
+      response = fetch("http://www.google.com/search?hl=en&q=#{URI.escape(query_parameter)}&filter=0&ie=utf-8&oe=utf-8")
 
       body = response.body.encode("UTF-8", invalid: :replace, undef: :replace, replace: "")
 
@@ -85,15 +89,34 @@ module Metrics
       # You should choose a better exception.
       raise ArgumentError, 'too many HTTP redirects' if limit == 0
 
-      response = Net::HTTP.get_response(URI(uri_str))
+      uri = URI(uri_str)
+      req = Net::HTTP::Get.new(uri)
+
+      req['User-Agent'] = 'ELinks/0.9.3 (textmode; Linux 2.6.9-kanotix-8 i686; 127x41)'
+      req['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+      req['Accept-Language'] = 'en-us,en'
+      req['Accept-Encoding'] = 'Accept-Encoding: deflate'
+      req['Cache-Control'] = 'max-age=0'
+
+      response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+        http.request(req)
+      end
 
       case response
       when Net::HTTPSuccess then
         response
       when Net::HTTPRedirection then
         location = response['location']
-        @logger.warn "redirected to #{location}"
-        fetch(location, limit - 1)
+
+        if location.index('sorry/IndexRedirect')
+          # was detected
+          @logger.warn "-- google detected bot activity, pause for 5 minutes"
+          sleep 5 * 60
+          fetch(uri_str)
+        else
+          @logger.warn "redirected to #{location}"
+          fetch(location, limit - 1)          
+        end
       else
         @logger.warn "-- Unknown response status: #{response.class}"
         response
